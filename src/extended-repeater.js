c2375c6705716bd9d1f22eca512f8f6ac0401237
Vladimir Zvonarenko <vzvonarenko@gmail.com>
2022-05-02 21:10:57 +0700

    feat: add finish Extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,11 +15,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+    if (options.separator === undefined) options.separator = '+';
+    if (options.additionSeparator === undefined) options.additionSeparator = '|';
+    if (options.addition === undefined) options.addition = '';
+    let result = '';
+    result += str;
+    while (options.additionRepeatTimes > 1) {
+        result += options.addition;
+        result += options.additionSeparator;
+        options.additionRepeatTimes--;
+    }
+
+    result += options.addition;
+    let temp = result;
+    while (options.repeatTimes > 1) {
+        result += options.separator;
+        result += temp;
+        options.repeatTimes--;
+    }
+
+    return result;
 }
 
 module.exports = {
-  repeater
+    repeater
 };
