8a74c7c39b40aabd2299329eecb28d45babe8279
Vladimir Zvonarenko <vzvonarenko@gmail.com>
2022-05-02 21:46:55 +0700

    feat: finish task mine-sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,11 +23,43 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+    let array = [];
+    for (let i = 0; i < matrix.length; i++) {
+        array.push([]);
+        for (let j = 0; j < matrix[i].length; j++) {
+            let bombs = 0;
+            if (i > 0 && j > 0 && matrix[i - 1][j - 1] == true) {
+                bombs += 1;
+            }
+            if (i > 0 && matrix[i - 1][j] == true) {
+                bombs += 1;
+            }
+            if (i > 0 && j < matrix[i].length - 1 && matrix[i - 1][j + 1] == true) {
+                bombs += 1;
+            }
+            if (j > 0 && matrix[i][j - 1] == true) {
+                bombs += 1;
+            }
+            if (j < matrix[i].length - 1 && matrix[i][j + 1] == true) {
+                bombs += 1;
+            }
+            if (i < matrix.length - 1 && j > 0 && matrix[i + 1][j - 1] == true) {
+                bombs += 1;
+            }
+            if (i < matrix.length - 1 && matrix[i + 1][j] == true) {
+                bombs += 1;
+            }
+            if (i < matrix.length - 1 && j < matrix[i].length - 1 && matrix[i + 1][j + 1] == true) {
+                bombs += 1;
+            }
+            array[i].push(bombs);
+        }
+    }
+
+    return array;
 }
 
 module.exports = {
-  minesweeper
+    minesweeper
 };
